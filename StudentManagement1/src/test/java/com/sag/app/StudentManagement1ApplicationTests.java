9b696bdf61aac3b73debf70f8e0b3edefc90289b
krashnat <52906982+krashnat@users.noreply.github.com>
2022-07-11 21:08:14 +0530

    this is for jenkins test

---
+++ StudentManagement1/src/test/java/com/sag/app/StudentManagement1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sag.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagement1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
